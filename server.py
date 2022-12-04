1283b5794c62465cf0d362a55dd43b3881b3329b
Justin Lau <justinwlau@gmail.com>
2022-12-04 13:46:08 -0800

    using only one template file

--- server.py
+++ server.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/play')
-def welcome_page():
-    return render_template("play.html")
+def play(times=3, color="cadetblue"):
+    return render_template("play_times_color.html", times=times, color=color)
 
 @app.route('/play/<int:times>')
-def play_times(times):
-    return render_template("play_times.html", times=times)
+def play_times(times=3, color="cadetblue"):
+    return render_template("play_times_color.html", times=times, color=color)
 
 @app.route('/play/<int:times>/<color>')
-def play_times_color(times, color):
+def play_times_color(times=3, color="cadetblue"):
     return render_template("play_times_color.html", times=times, color=color)
 
 if __name__ == "__main__":
